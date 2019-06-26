ece4ba1b05f59a69e486a97bf4ac985b9856bdbe
dac <dac@dypm.org>
2019-06-26 12:28:48 +0530

    first commit

---
+++ Customerinfo.js
@@ -0,0 +1,15 @@
+var express=require('express');
+var app=express();
+var customerinfo=function(req,res)
+{
+var product=[{
+var c_id:1,
+var C_Name:"SP",
+var C_AccountType:"Current"
+
+}]
+}
+res.send(product);
+
+app.get('/customer',customerinfo);
+var server=app.listen(2000);
